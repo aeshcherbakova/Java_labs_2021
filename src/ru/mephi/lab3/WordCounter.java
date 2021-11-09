c9943a6bd8f8c0520b1409224d6a0bed211b9eb8
aeshcherbakova <aeshcherbakova@yandex.ru>
2021-11-09 04:23:57 +0300

    lab3 task2 added

--- src/ru/mephi/lab3/WordCounter.java
+++ src/ru/mephi/lab3/WordCounter.java
@@ -6,12 +6,25 @@ import java.util.Scanner;
 public class WordCounter {
 
     public static LinkedHashMap<String, Integer> countWords(String input) {
-        LinkedHashMap<String, Integer> map = new LinkedHashMap();
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
         for(String word: input.split("[, :?.]+")) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
         return map;
     }
+
+    public static String deleteEqualWords(String input) {
+        String newStr = "";
+        HashSet<String> words = new HashSet<>();
+        for(String word: input.split("[, :?.]+")) {
+            if(!words.contains(word)) {
+                words.add(word);
+                newStr += " " + word;
+            }
+        }
+        return newStr;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
